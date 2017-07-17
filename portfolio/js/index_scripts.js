28592044787296f70df822b7a2081540845c65cc
w0218584 <w0218584@nscc.ca>
2017-07-17 16:05:16 -0300

    fixed minor issues with smaller screen sizes/ mobile

--- portfolio/js/index_scripts.js
+++ portfolio/js/index_scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
     $("#apps").hide();
     $("#about").hide();
+    $(".btn_proceed").hide();
 
 
     $("#app_btn").hover(
         function(){
-			console.log("blue");
+            $(this).find(".btn_proceed").fadeIn("slow");
+            if ($("#buttons").css("margin-top") == "200px"  || $(".chk_400").css("background-color") == "rgb(0, 0, 0)" ){
+               $(this).css("height", "120px")
+            }
             $("#intro").fadeOut(0, function(){
                 $("#apps").fadeIn(0);
             });
         }, function () {
+            $(this).find(".btn_proceed").fadeOut("slow");
+            if ($(this).css("height") == "120px" ){
+               $(this).css("height", "80px")
+            }
             $("#apps").fadeOut(0, function(){
                 $("#intro").fadeIn(0);
             });
@@ -18,10 +26,18 @@ $(document).ready(function() {
 
     $("#about_btn").hover(
         function(){
+            $(this).find(".btn_proceed").fadeIn("slow");
+            if ($("#buttons").css("margin-top") == "200px" ){
+               $(this).css("height", "120px")
+            }
             $("#intro").fadeOut(0, function(){
                 $("#about").fadeIn(0);
             });
         }, function () {
+            $(this).find(".btn_proceed").fadeOut("slow");
+            if ($(this).css("height") == "120px" ){
+               $(this).css("height", "80px")
+            }
             $("#about").fadeOut(0, function(){
                 $("#intro").fadeIn(0);
             });
